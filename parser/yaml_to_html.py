6b5a094fe0e809840471b48cae5001d276ceeac3
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2021-07-14 18:57:46 +0200

    Proper data format for project entries

--- parser/yaml_to_html.py
+++ parser/yaml_to_html.py
@@ -6,14 +6,30 @@ from tabulate import tabulate
 
 with open("../projects.yaml", "r") as stream:
     try:
-        data = yaml.safe_load(stream)
-        proj_list = []
-        for p in data["projects"]:
-            project = OpenSourceProject.from_dict(p)
-            print(project)
-            proj_list.append(project.to_list())
-        with open("table.html", 'w') as htmlfile:
-            htmlfile.write(tabulate(proj_list, tablefmt='unsafehtml', headers=OpenSourceProject.list_headers()))
+        with open("table.html", "w") as htmlfile:
+            htmlfile.write("<!DOCTYPE html>\n")
+            htmlfile.write("<html>\n")
+            htmlfile.write(
+                '<head>\n\
+                <link rel="stylesheet" type="text/css" href="table.css">\n\
+                </head>\n'
+            )
+            data = yaml.safe_load(stream)
+            for category in data:
+                proj_list = []
+                for p in data[category]:
+                    project = OpenSourceProject.from_dict(p)
+                    print(project)
+                    proj_list.append(project.to_list())
+                htmlfile.write(f"<h1>{category}</h1>\n")
+                htmlfile.write(
+                    tabulate(
+                        proj_list,
+                        tablefmt="unsafehtml",
+                        headers=OpenSourceProject.list_headers(),
+                    )
+                )
+            htmlfile.write("</html>\n")
 
     except yaml.YAMLError as exc:
         print("Error: Invalid yaml file:")
