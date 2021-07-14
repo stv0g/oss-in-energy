b0565e3d4380866bdd96393c19be4f78c229b3d1
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2021-07-14 15:42:22 +0200

    Added table creation for parser

--- parser/oss_project.py
+++ parser/oss_project.py
@@ -23,3 +23,10 @@ class OpenSourceProject:
             repo = "https://example.com"
 
         return cls(name=name, repository=repo, languages=None)
+
+    @classmethod
+    def list_headers(cls):
+        return ["Project", "Repository URL", "Languages"]
+
+    def to_list(self) -> List[str]:
+        return [self.name, self.repository , str(self.languages)]
--- parser/yaml_to_html.py
+++ parser/yaml_to_html.py
@@ -2,13 +2,18 @@
 
 import yaml
 from oss_project import OpenSourceProject
+from tabulate import tabulate
 
 with open("../projects.yaml", "r") as stream:
     try:
         data = yaml.safe_load(stream)
+        proj_list = []
         for p in data["projects"]:
             project = OpenSourceProject.from_dict(p)
             print(project)
+            proj_list.append(project.to_list())
+        with open("table.html", 'w') as htmlfile:
+            htmlfile.write(tabulate(proj_list, tablefmt='html', headers=OpenSourceProject.list_headers()))
 
     except yaml.YAMLError as exc:
         print("Error: Invalid yaml file:")
