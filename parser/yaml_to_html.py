faa9138037e1a962f3bf718afcaa83c206d4a69a
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2021-07-15 18:00:30 +0200

    Don't create header around html table

--- parser/yaml_to_html.py
+++ parser/yaml_to_html.py
@@ -7,13 +7,13 @@ from tabulate import tabulate
 with open("../projects.yaml", "r") as stream:
     try:
         with open("table.html", "w") as htmlfile:
-            htmlfile.write("<!DOCTYPE html>\n")
-            htmlfile.write("<html>\n")
-            htmlfile.write(
-                '<head>\n\
-                <link rel="stylesheet" type="text/css" href="table.css">\n\
-                </head>\n'
-            )
+            # htmlfile.write("<!DOCTYPE html>\n")
+            # htmlfile.write("<html>\n")
+            # htmlfile.write(
+            #     '<head>\n\
+            #     <link rel="stylesheet" type="text/css" href="table.css">\n\
+            #     </head>\n'
+            # )
             data = yaml.safe_load(stream)
             for category in data:
                 proj_list = []
@@ -29,7 +29,7 @@ with open("../projects.yaml", "r") as stream:
                         headers=OpenSourceProject.list_headers(),
                     )
                 )
-            htmlfile.write("</html>\n")
+            # htmlfile.write("</html>\n")
 
     except yaml.YAMLError as exc:
         print("Error: Invalid yaml file:")
