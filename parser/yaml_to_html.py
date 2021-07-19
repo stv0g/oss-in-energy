6b41dbd0f006e582acc009918bc38391a31364f9
Jonathan Klimt <jonathan.klimt@eonerc.rwth-aachen.de>
2021-07-19 15:55:06 +0200

    Parallelized list generation

--- parser/yaml_to_html.py
+++ parser/yaml_to_html.py
@@ -1,43 +1,60 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
+
 import yaml
+
 from oss_project import OpenSourceProject
 
 with open("../projects.yaml", "r") as stream:
     try:
         with open("table.html", "w") as htmlfile:
-            data = yaml.safe_load(stream)
-            for category in data:
-                proj_list = []
-                for p in data[category]:
-                    project = OpenSourceProject.from_dict(p)
-                    print(project)
-                    proj_list.append(project.to_list())
+            yaml_content = yaml.safe_load(stream)
 
+            raw_project_list = []
+
+            for category in yaml_content:
+                for proj in yaml_content[category]:
+                    raw_project_list.append((category, proj))
+
+            with ThreadPoolExecutor() as executor:
+                proj_list = executor.map(
+                    lambda cat_proj: (
+                        cat_proj[0],
+                        OpenSourceProject.from_dict(cat_proj[1]),
+                    ),
+                    raw_project_list,
+                )
+            projects = defaultdict(list)
+            for category, proj in proj_list:
+                projects[category].append(proj)
+
+            for category in projects.keys():
                 htmlfile.write(f"<h2>{category}</h2>\n")
 
                 htmlfile.write(f'<table style="table-layout: fixed; width: 250%">')
-                htmlfile.write(f'<thead>\n')
-                htmlfile.write(f'<tr>\n')
+                htmlfile.write(f"<thead>\n")
+                htmlfile.write(f"<tr>\n")
                 for header, style in OpenSourceProject.list_headers():
                     if style is not None:
                         htmlfile.write(f'<th style="{style}">{header}</th>\n')
                     else:
                         htmlfile.write(f"<th>{header}</th>\n")
-                htmlfile.write('</tr>\n')
-                htmlfile.write('</thead>\n')
+                htmlfile.write("</tr>\n")
+                htmlfile.write("</thead>\n")
 
                 htmlfile.write('<tbody style="font-size: 15px">\n')
-                for proj in proj_list:
-                    htmlfile.write(f'<tr>\n')
-                    for entry, style in proj:
+                for proj in projects[category]:
+                    htmlfile.write(f"<tr>\n")
+                    for entry, style in proj.to_list():
                         if style is not None:
                             htmlfile.write(f'<td style="{style}">{entry}</td>\n')
                         else:
                             htmlfile.write(f"<td>{entry}</td>\n")
-                    htmlfile.write(f'</tr>\n')
-                htmlfile.write('</tbody>\n')
-                htmlfile.write('</table>\n')
+                    htmlfile.write(f"</tr>\n")
+                htmlfile.write("</tbody>\n")
+                htmlfile.write("</table>\n")
 
     except yaml.YAMLError as exc:
         print("Error: Invalid yaml file:")
